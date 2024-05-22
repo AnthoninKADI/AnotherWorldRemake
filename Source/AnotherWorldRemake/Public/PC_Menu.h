bc3896674df623b5f16f921c9375232b49a4a3a3
Anthonin <akadi@artfx.fr>
2024-05-22 17:16:56 +0200

    [WIP] Menu & Pause Menu

---
+++ Source/AnotherWorldRemake/Public/PC_Menu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PC_Menu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANOTHERWORLDREMAKE_API APC_Menu : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
